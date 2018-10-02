c352fb11a2933b5c2e117cfe96580c827994c918
Vlad <volkov8991@gmail.com>
2018-10-03 01:00:48 +0300

    added output subcategories, but need front fix

--- wp-content/themes/kendall/woocommerce/archive-product.php
+++ wp-content/themes/kendall/woocommerce/archive-product.php
@@ -33,7 +33,6 @@ wp_enqueue_style(
     array(),
     null
 );
-//wp_enqueue_style( 'main.min1',get_template_directory_uri());
 ?>
 
 <section>
@@ -45,6 +44,27 @@ wp_enqueue_style(
 			</div>
 			<?php
 			woocommerce_product_loop_start();
+
+            $parentid = get_queried_object_id();
+            $args = array(
+                'parent' => $parentid
+            );
+            $terms = get_terms( 'product_cat', $args );
+            if ( $terms ) {
+                echo '<ul class="product-cats">';
+                foreach ( $terms as $term ) {
+                    echo '<li class="category">';
+                    woocommerce_subcategory_thumbnail( $term );
+                    echo '<h2>';
+                    echo '<a href="' .  esc_url( get_term_link( $term ) ) . '" class="' . $term->slug . '">';
+                    echo $term->name;
+                    echo '</a>';
+                    echo '</h2>';
+                    echo '</li>';
+                }
+                echo '</ul>';
+            }
+
 			?>
             <div class="slider">
                 <img class="product_BG" src="img/product_BG.png" alt="">
